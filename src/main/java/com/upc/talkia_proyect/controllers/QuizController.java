59edcefd6dd77d496760313733433e366fa26203
unknown <u202217059@upc.edu.pe>
2024-09-19 23:45:41 -0500

    feat(backend): Add Quiz DTO for visualization

--- src/main/java/com/upc/talkia_proyect/controllers/QuizController.java
+++ src/main/java/com/upc/talkia_proyect/controllers/QuizController.java
@@ -1,12 +1,15 @@
 package com.upc.talkia_proyect.controllers;
 
+import com.upc.talkia_proyect.dtos.QuizDTO;
 import com.upc.talkia_proyect.entities.Quiz;
 import com.upc.talkia_proyect.services.QuizService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,9 +17,15 @@ import java.util.List;
 public class QuizController {
     @Autowired
     private QuizService quizService;
-
+    ModelMapper modelMapper=new ModelMapper();
     @GetMapping("/quizzes")
-    public List<Quiz> listQuizzes(){
-        return null;
+    public List<QuizDTO> listQuizzes(){
+        List<Quiz> quizzes =quizService.listQuizzes();
+        List<QuizDTO> quizDTOs=modelMapper.map(quizzes,List.class);
+        return quizDTOs;
     }
+    
+
+
+
 }
